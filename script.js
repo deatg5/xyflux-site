9ac281f9595e2e4cc14c210f89c7542d80288ee8
deatg5 <67492418+deatg5@users.noreply.github.com>
2021-09-26 23:48:05 -0600

    Created the project, finished navbar

---
+++ script.js
@@ -0,0 +1,21 @@
+function ToggleNavMenu() {
+    var x = document.getElementById("hidden-menu");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+    //start checking if the screen width is over 1050
+    setInterval(RemoveHiddenMenu, 100)
+}
+
+//the hidden menu remains after changing the width above 1050px after revealing this navbar, so just check if the width is over 1050 and set hidden menu display to none if it is
+//(hopefully i find a different way of doing this)
+function RemoveHiddenMenu() {
+    var w = parseInt(window.innerWidth);
+
+    if (w >= 1050) {
+        document.getElementById("hidden-menu").style.display = "none";
+    }
+}
+        
